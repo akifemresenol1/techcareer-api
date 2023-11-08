fb3cfe6b57bdf0ef124c5467e7f268694f224cde
Akif Emre Senol <akifffemreee@gmail.com>
2023-11-09 01:59:51 +0300

    add CORS

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const port = 3001;
 
 app.use(express.json());
 
+// CORS başlıklarını ekleyin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  // Diğer CORS başlıklarını da ayarlayabilirsiniz.
+  next();
+});
+
 app.use(express.static("public"));
 
 // Endpoint dosyaları dahil edildi
